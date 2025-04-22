2f13e0c79c866031cb95bf50c6b639c8d97210cd
Juan Pérez <juancruz041@gmail.com>
2025-04-22 19:30:41 -0300

    test db

--- src/index.js
+++ src/index.js
@@ -1,18 +1,35 @@
 const express = require('express');
 const cors = require('cors');
+const { Pool } = require('pg');
 require('dotenv').config();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
-
 app.use(cors());
 app.use(express.json());
 
-//prueba
-app.get('/', (req, res) => {
-  res.send('E-commerce Backend running ✅');
+const pool = new Pool({
+  user: 'admin',
+  host: 'dpg-d038d1ruibrs738578dg-a',
+  database: 'ecommercebackendbd',
+  password: 'gVOTfd6qSboZ5PrrkqxNQPSF9hdaAKY0',
+  port: 5432,
+  ssl: { rejectUnauthorized: false }
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+// Endpoint test
+app.get('/test-db', async (req, res) => {
+  try {
+    const result = await pool.query('SELECT NOW()');
+    res.json({ status: '✅ Conexión exitosa', hora: result.rows[0].now });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ status: '❌ Error al conectar', error: err.message });
+  }
 });
+
+// server up 
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Servidor escuchando en el puerto ${PORT}`);
+});
+
